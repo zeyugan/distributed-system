af9ea8bd5c9f2eaf1913dcd629c8e77b9c367f51
Gan Ze Yu <52880557+zeyugan@users.noreply.github.com>
2023-10-01 16:02:26 +0800

    misc

--- server/Server.java
+++ server/Server.java
@@ -5,24 +5,24 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
-public class server {
+public class Server {
 
     static final Integer PORT = 1234;
     static final Integer BUFFER_SIZE = 65535;
 
 
     public static void main(String[] args) throws IOException {
-        try {
-            // Step 1 : Create a socket to listen at port 1234
-            DatagramSocket ds = new DatagramSocket(PORT);
-            byte[] receive = new byte[BUFFER_SIZE];
 
-            DatagramPacket dpReceive = null;
-            while (true) {
+        // create a socket on port
+        DatagramSocket ds = new DatagramSocket(PORT);
+        byte[] receive = new byte[BUFFER_SIZE];
 
+        DatagramPacket dpReceive = null;
+        while (true) {
+            try {
                 // parse received packet
                 dpReceive = new DatagramPacket(receive, BUFFER_SIZE);
-                ds.receive(dpReceive);
+                ds.receive(dpReceive); // blocking
 
                 System.out.println("Client:-" + data(receive));
 
@@ -33,21 +33,22 @@ public class server {
 
                 // Clear the buffer after every message.
                 receive = new byte[BUFFER_SIZE];
+            } catch (Exception e) {
+                System.out.println("Error occurred while receiving packet.");
+                e.printStackTrace();
+                receive = new byte[BUFFER_SIZE];
             }
-
-            System.out.println("Shutting down server");
-            ds.close();
-            System.exit(0);
-
-        } catch (Exception e) {
-            System.out.println("Server Crashed");
-            e.printStackTrace();
-            System.exit(1);
         }
+
+        System.out.println("Shutting down server");
+        ds.close();
+        System.exit(0);
+
     }
 
     // A utility method to convert the byte array
     // data into a string representation.
+    // todo unmarshal data
     public static StringBuilder data(byte[] a) {
         if (a == null)
             return null;
